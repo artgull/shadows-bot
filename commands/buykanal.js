e44683d96b87ccc0f259a5ad2d57880a4c5c8c2a
artgull <34341540+artgull@users.noreply.github.com>
2020-03-16 18:56:41 +0600

    1.0.6.1

--- commands/buykanal.js
+++ commands/buykanal.js
@@ -8,20 +8,21 @@ module.exports.run = (bot, message, args) => {
     let cat = '435499299137257499';
     let catneshad = '648511794926583808';
     let name = args[2];
-    let namech = args[2];
+    let namech = args[1];
     let rcheck = message.guild.member(author);
     message.delete(1);
-     if (args[1] === "канал" || "уникальный голосовой канал") {
-        if (args[2] === undefined) return message.channel.send("Не указано название канала. Правильное использование `-buy канал имя канала`");
+    console.log(args)
+     //if (args[1] === "канал" || "уникальный голосовой канал") {
+        if (args[1] === undefined) return message.channel.send("Не указано название канала. Правильное использование `-buy-канал имя канала`");
         if(coins[author].coins < 1000000) return message.channel.send("Недостаточно душ для покупки");
         message.guild.createChannel(namech, 'voice').then(m => {
             m.setParent(cat);
         })
         coins[author].coins = coins[author].coins - 1000000;
-        message.reply(`Вы успешно купили канал **${name}**!`);
-    }
+        message.reply(`Вы успешно купили канал **${namech}**!`);
+    //}
 }
 
 module.exports.help = {
-    name: "buy канал"
+    name: "buy-канал"
 }
