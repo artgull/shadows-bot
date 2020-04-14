8d3f0527396453f540ee75b68f91be011b953fd9
artgull <34341540+artgull@users.noreply.github.com>
2020-04-14 23:14:06 +0600

    1.1.0.0.0.0.1

--- commands/xpgive.js
+++ commands/xpgive.js
@@ -9,7 +9,7 @@ const Stat = require("../models/stats.js");
 
 module.exports.run = async (bot, message, args) => {
   message.delete(1);
-  if (message.member.roles.has('435764899369713664')) {
+  if (message.member.roles.has('435764899369713664' || '648464327636156416')) {
   let pUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[1])
   Stat.findOne({
     userID: pUser.id
