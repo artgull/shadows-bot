688247184c308d8a61234f960a3ae2c95e379108
artgull <34341540+artgull@users.noreply.github.com>
2020-04-17 13:59:23 +0600

    1.1.3.1

--- commands/shop.js
+++ commands/shop.js
@@ -2,14 +2,17 @@ const Discord = require("discord.js")
 
 module.exports.run = async (bot, message, args) => {
     message.delete(1);
-    let arh = "487921687258595351";
+    let arh = message.guild.roles.get('487921687258595351');
     let shopembed = new Discord.RichEmbed()
     .setTitle("**Магазин**")
-    .setDescription("Здесь вы можете купить какой нибудь прикол")
+    .setDescription(`
+
+    **Уникальный голосовой канал**, 1000000 👻
+    ${arh.toString()}, 500 👻
+    
+    `)
     .setThumbnail('https://cdn.discordapp.com/attachments/681409687039901726/686235348480950292/botava.png')
     .setColor("#4169e1")
-    .addField("**Уникальный голосовой канал**", "1000000 👻")
-    .addField("<@&" + arh + ">", "500 👻", true)
     .setFooter("Используйте -buy для покупки")
     //.addField(message.guild.member, "3000 👻")
     message.channel.send(shopembed);
