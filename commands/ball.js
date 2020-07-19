32a9fe865cba23dd9965d668e130f1edcc3543c8
artgull <34341540+artgull@users.noreply.github.com>
2020-07-20 02:02:47 +0600

    Update ball.js

--- commands/ball.js
+++ commands/ball.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js')
 
 module.exports.run = (bot, message, args) => {
-    message.delete()
+    //message.delete()
     let words = ['Да', 'Нет', 'Возможно', 'Не точно', 'Может быть', 'Спроси Игоря', 'Конечно!']
     var rand = Math.floor(Math.random() * words.length)
    // if(args[1] === undefined) return message.reply("А где слова чтобы я мог гадать?")
