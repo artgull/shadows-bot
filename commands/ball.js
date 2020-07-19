28c8d02235f07a8fcb2ea1dca2c68f78bc526e5c
artgull <34341540+artgull@users.noreply.github.com>
2020-07-20 02:41:32 +0600

    Update ball.js

--- commands/ball.js
+++ commands/ball.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js')
 
 module.exports.run = (bot, message, args) => {
     //message.delete()
-    let words = ['Да', 'Нет', 'Возможно', 'Не точно', 'Может быть', 'Спроси Игоря', 'Конечно!']
+    let words = ['Да', 'Нет', 'Возможно', 'Не точно', 'Может быть', 'Спроси Игоря', 'Конечно!', 'Вопрос вне моей компетенции', 'Вопрос интимного характера', 'Ваша банковская карта заблокирована ЦБ РФ тел.8(495)1017407(беспл.РФ)', 'Этот абонент недавно появился в сети']
     var rand = Math.floor(Math.random() * words.length)
    // if(args[1] === undefined) return message.reply("А где слова чтобы я мог гадать?")
     message.channel.send(words[rand])
