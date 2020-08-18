1344ff7d744f18d459e527048f0aad87602c56a9
artgull <34341540+artgull@users.noreply.github.com>
2020-08-19 01:13:33 +0600

    fix

--- models/shop.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const shopSchema = mongoose.Schema({
-    sellerID: String,
-    sellerName: String,
-    guildid: String,
-    item: String,
-    price: Number
-})
-module.exports = mongoose.model("Shop", shopSchema);
